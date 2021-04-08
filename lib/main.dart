cede1aebca871829f50ae352793bca1b2e0011d2
Depanri Purba <depanri.purba@gmail.com>
2021-04-08 21:15:00 +0700

    comit agar bisa switch branch

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
       initialRoute: '/',
       routes: {
         '/': (context) => MyApp(),
+         '/Halamandua': (context) => Halamandua(),
       }));
 }
 
@@ -15,11 +16,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(title: new Text("Depanri-DataTable")),
-        body: new Column(
-          children: <Widget>[
-            new Center(
-              child: new Text("Tabel Mahasiswa", style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        body: new Column(children: <Widget>[
+          new Center(
+            child: new Text(
+              "Tabel Mahasiswa",
+              style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+          ),
           new DataTable(
             columns: [
               DataColumn(label: Text('NIRM')),
@@ -32,19 +35,42 @@ class MyApp extends StatelessWidget {
                 DataCell(Text('Depanri Purba')),
                 DataCell(Text('Komputer')),
               ]),
-               DataRow(cells: [
-              DataCell(Text('2018020019')),
-              DataCell(Text('Vina Al Fitriani')),
-              DataCell(Text('Hukum')),
-            ]),
-            DataRow(cells: [
-              DataCell(Text('2018021014')),
-              DataCell(Text('Dien Ronatio Tambunan')),
-              DataCell(Text('Ekonomi')),
-            ]),
+              DataRow(cells: [
+                DataCell(Text('2018020019')),
+                DataCell(Text('Vina Al Fitriani')),
+                DataCell(Text('Hukum')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('2018021014')),
+                DataCell(Text('Dien Ronatio Tambunan')),
+                DataCell(Text('Ekonomi')),
+              ]),
             ],
           ),
-          new Center(child: new Text("Halaman 2"),)
+          new Center(
+            child: new TextButton(
+              child: new Text("Berikutnya"),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Halamandua");
+              },
+            ),
+          )
         ]));
   }
+}
+
+class Halamandua extends StatelessWidget{
+  @override
+   Widget build(BuildContext context){
+     return new Scaffold(
+       appBar: new AppBar(title: new Text("halaman 2"),),
+       body: new ListView(
+         children: <Widget>[
+           new Center(
+             child: new Text("Tabel Bagian dua"),
+             )
+         ],
+       ),
+     );
+   }
 }

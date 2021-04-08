d723e9c19585079d62ff6c9eb3b6009a1d5b9128
Depanri Purba <depanri.purba@gmail.com>
2021-04-08 11:09:14 +0700

    menambahkan widget datatabel

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title:'routing navigation',
-    initialRoute: '/',
-    routes: {
-      '/':(context)=>HalamanPertama(),
-      HalamanKedua.routeName:(context)=>HalamanKedua(),
-      HalamanKetiga.routeName:(context)=>HalamanKetiga(),
-      
-    },
-    ),);
+      debugShowCheckedModeBanner: false,
+      title: "DataTable Depanri",
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyApp(),
+      }));
 }
 
-class HalamanPertama extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Halaman Pertama"),
-      ),
-      body: Center(
-        child: GridView.count(
-          crossAxisCount: 2,
+    return new Scaffold(
+        appBar: new AppBar(title: new Text("DataTable")),
+        body: new Column(
           children: <Widget>[
-            RaisedButton(
-              child: Text("Pindah ke halaman dua"),
-              onPressed: (){
-                Navigator.pushNamed(context, HalamanKedua.routeName);
-              },
+         new DataTable(
+           columns: [
+            DataColumn(
+              label: new Text("Nama barang"),
+              numeric: false
             ),
-            RaisedButton(
-              child: Text("Pindah ke halaman tiga"),
-              onPressed: (){
-                Navigator.pushReplacementNamed(context, HalamanKetiga.routeName);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+            DataColumn(
+              label: new Text("Harga"),
+              numeric: true
+            )
+           ],
+           rows: [],
+         )
+        ]
+        )
+        );
   }
 }
-class HalamanKedua extends StatelessWidget{
-  static const String routeName = "/halamanKedua";
- @override
- Widget build(BuildContext context){
-   return Scaffold(
-    appBar:AppBar(
-      title: Text("ini adalah halaman kedua"),
-      ),
-      body:Center(
-        child: RaisedButton(
-          child: Text("kembali"),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
-      ),
-  );
-  }
-}
-class HalamanKetiga extends StatelessWidget{
-  static const String routeName = "/halamanKetiga";
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Halaman Ketiga"),
-      ),
-      body:Center(
-        child: Text("ini adalah halaman ketiga"),
-      ) ,
-      );
-  }
-}
+
+
